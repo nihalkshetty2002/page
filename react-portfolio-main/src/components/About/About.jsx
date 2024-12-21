c6d92c19d0e359829fe5660033aa0de05ba31710
Nihal <nihalshetty54@gmail.com>
2024-12-21 21:01:27 +0530

    uiicon

--- react-portfolio-main/src/components/About/About.jsx
+++ react-portfolio-main/src/components/About/About.jsx
@@ -35,7 +35,7 @@ export const About = () => {
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <img src={getImageUrl("about/uiIcon.png")} alt="UI icon" />
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
               <p>
