50e7ad535f0b273c429ef9449b59b35bd0417c8d
Nihal <nihalshetty54@gmail.com>
2024-12-23 18:57:39 +0530

    updated navbar component

--- react-portfolio-main/src/components/Navbar/Navbar.jsx
+++ react-portfolio-main/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
-
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  // Toggle menu open/close
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  // Close menu when clicking on a link
+  const closeMenu = () => setMenuOpen(false);
+
   return (
     <nav className={styles.navbar}>
+      {/* Navbar Title */}
       <a className={styles.title} href="/">
         Portfolio
       </a>
+
+      {/* Menu Section */}
       <div className={styles.menu}>
+        {/* Menu Button (Hamburger Icon for Mobile) */}
         <img
           className={styles.menuBtn}
           src={
             menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
+              ? getImageUrl("nav/closeIcon.png") // Close Icon
+              : getImageUrl("nav/menuIcon.png")  // Menu Icon
           }
           alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
+          onClick={toggleMenu}
         />
+
+        {/* Menu Items */}
         <ul
           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-          onClick={() => setMenuOpen(false)}
+          onClick={closeMenu}
         >
           <li>
             <a href="#about">About</a>
